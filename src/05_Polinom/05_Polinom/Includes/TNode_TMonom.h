f70a2f598cfff159d01f7be4b47d8eca14c5cab1
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2019-11-17 14:31:41 +0300

    specialization of classes tnode and tlist

---
+++ src/05_Polinom/05_Polinom/Includes/TNode_TMonom.h
@@ -0,0 +1,26 @@
+#ifndef _TNODE_TMONOM_H_
+#define _TNODE_TMONOM_H_
+
+#include "exceptions.h"
+#include "TNode.h"
+
+template<>
+class TNode <int, float>
+{
+public:
+	int key;  // degree
+	float pData;  // coeff
+	TNode<int, float>* pNext;
+
+	TNode() : key(0), pData(0), pNext(NULL) {}
+	TNode(int _key, float _data, TNode* _node = NULL) : key(_key), pData(_data), pNext(_node) {}
+	TNode(const TNode& _copy) : key(_copy.key), pData(_copy.pData), pNext(_copy.pNext) {}
+	~TNode()
+	{
+		key = 0;
+		pData = 0;
+		pNext = NULL;
+	}
+};
+
+#endif
